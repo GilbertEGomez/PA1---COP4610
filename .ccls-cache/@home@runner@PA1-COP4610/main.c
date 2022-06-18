da3591261d03d41acbb686dfc7b1f2986c85f77b
Nosgnille05 <98973755+nosgnille05@users.noreply.github.com>
2022-06-18 20:17:33 +0000

    Finalized FIFO, Addition of SJF

--- .ccls-cache/@home@runner@PA1-COP4610/main.c
+++ .ccls-cache/@home@runner@PA1-COP4610/main.c
@@ -1,5 +1,5 @@
 /*
-	PA1-COP4610
+	PA1-Temp
 */
 #include "pa1.h"
 #include <math.h>
@@ -11,7 +11,7 @@
 #define nrand() (sqrt(-2*log(frand()))*cos(2*M_PI*frand()))
 heap generate_arrival_times(int n, int k){
   heap h = create_heap(n);
-  for(int i = 0; i < n;i++)
+  for(int i = 0; i < n; i++)
     min_insert(&h, frand()*k);
   return h;
 }
@@ -23,12 +23,12 @@ int main(int argc, char **argv)
 	extern char *optarg;
 	extern int optind;
 	int c, err = 0; 
-	int nflag = 0, kflag = 0;
-  int nval, kval, dval;
+	int nflag = 0, kflag = 0, dflag = 0;
+  int nval, kval, dval=1;
   
-	static char usage[] = "usage: %s -n nval -k kval\n";
+	static char usage[] = "usage: %s -n nval -k kval [-d] dval\n";
 
-	while ((c = getopt(argc, argv, "n:k:")) != -1)
+	while ((c = getopt(argc, argv, "n:k:d:")) != -1)
 		switch (c) {
 		case 'n':
 			nflag = 1;
@@ -37,6 +37,10 @@ int main(int argc, char **argv)
 		case 'k':
 			kflag = 1;
       kval = atoi(optarg);
+			break;
+    case 'd':
+			dflag = 1;
+      dval = atoi(optarg);
 			break;
 		case '?':
 			err = 1;
@@ -49,20 +53,21 @@ int main(int argc, char **argv)
 		exit(1);
 	}
   
-	/* print flags and there values */
-	printf("nflag = %d\n", nflag);
+	/* print flags with their values */
+	/*
+  printf("nflag = %d\n", nflag);
   printf("nval = %d\n", nval);
   printf("kflag = %d\n", kflag);
 	printf("kval = %d\n", kval);
+  printf("dflag = %d\n", dflag);
+	printf("dval = %d\n", dval);
+  */
 
-  /* do something useful with the cmdline arguments */
-  for (int dcount = 1; dcount < 26; dcount++){
-  dval = dcount*kval/nval;
-  /* add FIFO, SJF, SRT */
-  double v = dval/4.0;
+  int d_val = dval*kval/nval;//d...25d
+  
+  /*FIFO*/
+  double v = d_val/4.0;
   heap arrival_times = generate_arrival_times(nval,kval);
-    
-  /*FIFO START*/
   lnklst_queue queue = create_queue2();
   int t = 0;
   double att = 0.0;
@@ -72,7 +77,7 @@ int main(int argc, char **argv)
       process p;
       p.arrival_time = t;
       p.remaining_time = p.burst_time = 
-        (int)round(nrand()*v + dval);
+        (int)round(nrand()*v + d_val);
       p.tt = 0;
       p.priority_level = rand()%10 + 1;
       enqueue2(&queue, p);
@@ -93,27 +98,70 @@ int main(int argc, char **argv)
     }
     t++;
   }
-  printf("FIFO Algorithm for (n,k)=(%d,%d): ATT= %.3f,   d= %d, d/ATT= %.3f\n", nval, kval, att/nval, dval, dval*nval/att);
+  printf("FIFO Algorithm for (n,k)=(%d,%d): ATT= %.3f,   d= %d, d/ATT= %.4f\n", nval, kval, att/nval, d_val, d_val*nval/att);
   /*END FIFO*/
 
   /*SJF*/
-  double sjf_v = dval/4.0;
+  double sjf_v = d_val/4.0;
   heap sjf_arrival_times = generate_arrival_times(nval,kval);
   lnklst_queue sjf_queue = create_queue2();
+  int curr_at, next_at, curr_bt, next_bt;
   int sjf_t = 0;
-  double sjf_att = 0.0;
+  double sjf_att = 0.0;//sum of all turnaround times
   process * sjf_current = NULL;
   while(!sjf_current || sjf_t < kval || !is_empty2(sjf_queue)){
-    while(sjf_t == get_min(sjf_arrival_times)){
+    while(sjf_t == get_min(sjf_arrival_times)){ //get min arrival time
       process p;
-      p.arrival_time = sjf_t;
-      p.remaining_time = p.burst_time = 
-        (int)round(nrand()*sjf_v + dval);
-      p.tt = 0;
-      p.priority_level = rand()%10 + 1;
-      enqueue2(&sjf_queue, p);
+      curr_at = sjf_t;
       min_delete(&sjf_arrival_times);
-    }
+      next_at = get_min(sjf_arrival_times);
+      
+        if(curr_at == next_at){//curr and next arrive simultaneously, give each bt and compare
+          curr_bt = (int)round(nrand()*sjf_v + d_val);
+          next_bt = (int)round(nrand()*sjf_v + d_val);
+          if(curr_bt <= next_bt){
+            //admit curr_at
+            p.remaining_time = p.burst_time = curr_bt;
+            p.arrival_time = sjf_t;
+            p.tt = 0;
+            p.priority_level = rand()%10 + 1;
+            enqueue2(&sjf_queue, p);
+            min_delete(&sjf_arrival_times);
+            //then admit next_at
+            p.remaining_time = p.burst_time = next_bt;
+            p.arrival_time = sjf_t;
+            p.tt = 0;
+            p.priority_level = rand()%10 + 1;
+            enqueue2(&sjf_queue, p);
+            min_delete(&sjf_arrival_times);
+          }
+          else{
+            //admit next_at
+            p.remaining_time = p.burst_time = next_bt;
+            p.arrival_time = sjf_t;
+            p.tt = 0;
+            p.priority_level = rand()%10 + 1;
+            enqueue2(&sjf_queue, p);
+            min_delete(&sjf_arrival_times);
+            //then admit curr_at
+            p.remaining_time = p.burst_time = curr_bt;
+            p.arrival_time = sjf_t;
+            p.tt = 0;
+            p.priority_level = rand()%10 + 1;
+            enqueue2(&sjf_queue, p);
+            min_delete(&sjf_arrival_times);
+          }
+        }
+        else{//curr arrives alone, give bt, admit
+          //admit curr_at
+          p.remaining_time = p.burst_time = (int)round(nrand()*sjf_v + d_val);
+          p.arrival_time = sjf_t;
+          p.tt = 0;
+          p.priority_level = rand()%10 + 1;
+          enqueue2(&sjf_queue, p);
+          min_delete(&sjf_arrival_times);
+        }
+      }
     if(sjf_current == NULL && !is_empty2(sjf_queue)){
       sjf_current = (process*)malloc(sizeof(process));
       *sjf_current = dequeue2(&sjf_queue);
@@ -122,15 +170,14 @@ int main(int argc, char **argv)
       sjf_current->remaining_time--;
       if(sjf_current->remaining_time == 0){
         sjf_current->tt = (sjf_t+1) - sjf_current->arrival_time;
-        sjf_att += sjf_current->tt;        
+        sjf_att = sjf_att + sjf_current->tt;   
         free(sjf_current);
         sjf_current = NULL;
       }
     }
     sjf_t++;
   }
-  printf("SJF  Algorithm for (n,k)=(%d,%d): ATT= %.3f, d= %d, d/ATT= %.3f\n\n", nval, kval, sjf_att, dval, dval*nval/sjf_att);
-  }//dcount
+  printf("SJF  Algorithm for (n,k)=(%d,%d): ATT= %.3f, d= %d, d/ATT= %.4f\n", nval, kval, sjf_att/nval, d_val, d_val*nval/sjf_att);
   /*END SJF*/
   
   exit(0);
